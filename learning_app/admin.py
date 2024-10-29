076060ec8012cdb5c1bf538be5a079f53abafee7
RafaelEmery <rafael.emerycade@gmail.com>
2024-10-29 07:43:52 -0300

    feat: adding models and migrations

--- learning_app/admin.py
+++ learning_app/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from .models import Course, Review
+
+
 # Register your models here.
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'url', 'published_at', 'updated_at', 'active']
+    
+    # Other sugestions:
+    # list_filter = ['active', 'published_at']
+    # search_fields = ['title', 'url']
+    # date_hierarchy = 'published_at'
+    # ordering = ['published_at']
+    
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['course', 'name', 'email', 'rating', 'published_at', 'updated_at', 'active']
+    
